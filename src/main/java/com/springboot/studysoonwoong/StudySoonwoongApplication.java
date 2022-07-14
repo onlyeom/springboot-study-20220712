de582f787476bfc2535e18384d12755d07014e2a
onlyeom <onlyeom@gmail.com>
2022-07-14 21:47:12 +0900

    init commit

---
+++ src/main/java/com/springboot/studysoonwoong/StudySoonwoongApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.studysoonwoong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySoonwoongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySoonwoongApplication.class, args);
+	}
+
+}
